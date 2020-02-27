50930c5247a864d0601f15a3b04dfe536f545c80
Bogdan Stefanescu <bogdan@quandora.com>
2020-02-27 09:13:25 +0100

    implemented a q:model directive useable by custom form controls

---
+++ plugins/form/src/control-model.js
@@ -0,0 +1,39 @@
+import Qute from '@qutejs/runtime';
+
+// impl a q:model that can be used by components implementing custom form controls
+// the requirement for the component using this directive is to provide
+// a `value` property and to trigger a `change` event when the value changes
+
+export default function controlModelDirective(xattrs, valueExpr, compOrEl) {
+    var rendering = this, value, boundProp = (typeof valueExpr === 'string') ? valueExpr : null;
+
+    if ( !(Qute.isVM(compOrEl) && ("value" in compOrEl)) ) {
+        throw new Error('Only ViewModel components with a "value" property can use q:model');
+    }
+
+    if (boundProp && !rendering.vm) {
+        // current component not a ViewModel - throw an error
+        throw new Error('q:model bound to "'+valueExpr+'" property but the current component is not a ViewModel component!');
+    }
+
+    function updateValue() {
+        var newValue = boundProp ? rendering.vm[boundProp] : rendering.eval(valueExpr);
+        compOrEl.value = newValue;
+    }
+
+    // we need to update the value now
+    updateValue();
+    rendering.up(function() { updateValue() });
+
+    return function(el) {
+        if (boundProp) {
+            // then automatically update prop when changed
+            console.log('!!!!!!!!!!!', el);
+            el.addEventListener('change', function(e) {
+                console.log('@@@@@@@@@@@@@@@@@changed')
+                rendering.vm[boundProp] = compOrEl.value;
+            });
+        }
+    }
+
+}
--- plugins/form/src/index.js
+++ plugins/form/src/index.js
@@ -2,6 +2,7 @@ import Qute from '@qutejs/runtime';
 import {formValidateDirective, validationMessageDirective, inputValidationDirective} from './validation.js';
 import inputModelDirective from './input-model.js';
 import selectModelDirective from './select-model.js';
+import controlModelDirective from './control-model.js';
 
 Qute.registerDirective('form', 'validate', formValidateDirective);
 Qute.registerDirective('input', 'model', inputModelDirective);
@@ -11,3 +12,15 @@ Qute.registerDirective('input', 'validate', inputValidationDirective);
 Qute.registerDirective('textarea', 'validate', inputValidationDirective);
 Qute.registerDirective('select', 'validate', inputValidationDirective);
 Qute.registerDirective('validation-message', validationMessageDirective);
+
+var FormApi = {
+	// Enable the q:model directive on a custom form control
+	// The control must be implemented as a ViewModel component and must provide a 'value' reactive property.
+	// For bidirectional updates it must also trigger a change event when the control value is changed.
+	registerControl: function(tag) {
+		Qute.registerDirective(tag, 'model', controlModelDirective);
+	}
+}
+//TODO use Qute.registerModule('form', FormApi) which in prod does nothing
+Qute.$.form = FormApi;
+export default FormApi;
