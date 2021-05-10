13ddbe82c741cb7d6620547d0f7bedbcf18c5db5
Bogdan Stefanescu <bogdan@quandora.com>
2021-05-10 16:20:26 +0200

    be able to change the snapshot directory trough env vars

--- tools/test-utils/src/snapshot.js
+++ tools/test-utils/src/snapshot.js
@@ -2,7 +2,7 @@ import { join, dirname } from 'path';
 import fs from 'fs';
 import assert from 'assert';
 
- function getCallerFile() {
+function getCallerFile() {
  	var orig = Error.prepareStackTrace;
  	Error.prepareStackTrace = function(_, stack){ return stack; };
  	var stack = new Error().stack;
@@ -13,9 +13,19 @@ import assert from 'assert';
 	return stack[2].getFileName();
 };
 
+/**
+ * By default the snapshot dir is named 'snapshots' and is located in the same dir as the calling test file.
+ * If you need to change the directory location then uyou can use the QUTEJS_TEST_SNAPSHOTS_DIR env variable to set it:
+ *
+ * process.env.QUTEJS_TEST_SNAPSHOTS_DIR = '/my/snapshots/dir';
+ *
+ * @param {*} id
+ * @param {*} content
+ * @param {*} ignoreSpaces
+ */
 export default function snapshot(id, content, ignoreSpaces) {
 	if (ignoreSpaces) content = content.trim().replace(/>\s+/g, '>').replace(/\s+</g, '<');
-	var snapshotsDir = join(dirname(getCallerFile()), 'snapshots');
+	var snapshotsDir = process.env.QUTEJS_TEST_SNAPSHOTS_DIR || join(dirname(getCallerFile()), 'snapshots');
 	var snapshotFile = join(snapshotsDir, id);
 	if (!fs.existsSync(snapshotsDir)) fs.mkdirSync(snapshotsDir);
 	if (!fs.existsSync(snapshotFile)) {
